3fd3a0c54e67a0ba912e4b515069a890ec01aea9
InternetBowser <internetbowsergd@gmail.com>
2026-02-09 18:28:13 -0600

    Added containerization

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
+import figlet from 'figlet';
+import index from './index.html'; 
+
 const server = Bun.serve({
   port: 6741,
   routes: {
-    "/": () => new Response('Bun!'),
+    "/": index, 
+    "/figlet": () => {
+      const body = figlet.textSync('Bun!');
+      return new Response(body);
+    }
   }
 });
 
