7c51c6449383a10b0abd63d11cdbf50e439d62c3
InternetBowser <internetbowsergd@gmail.com>
2026-02-09 21:34:17 -0600

    Add user count WS management

--- index.ts
+++ index.ts
@@ -1,15 +1,47 @@
-import figlet from 'figlet';
-import info from './static/info.html'
+import info from "./static/info.html";
 
-const server = Bun.serve({
+interface SocketData {
+  route: string;
+}
+
+const server = Bun.serve<SocketData>({
   port: 6741,
   routes: {
-    "/": info, 
-    "/figlet": () => {
-      const body = figlet.textSync('Bun!');
-      return new Response(body);
+    "/": info,
+  },
+  fetch(req, server) {
+    const url = new URL(req.url);
+    if (url.pathname === "/live-user-count") {
+      const route = url.searchParams.get("route") || "global";
+      const upgraded = server.upgrade(req, { data: { route } });
+      if (upgraded) return undefined;
     }
-  }
+    return new Response("Not Found", { status: 404 });
+  },
+  websocket: {
+    open(ws) {
+      const { route } = ws.data;
+      ws.subscribe(route);
+      server.publish(
+        route,
+        JSON.stringify({
+          count: server.subscriberCount(route),
+        }),
+      );
+    },
+    close(ws) {
+      const { route } = ws.data;
+      ws.unsubscribe(route);
+
+      server.publish(
+        route,
+        JSON.stringify({
+          count: server.subscriberCount(route),
+        }),
+      );
+    },
+    message() {},
+  },
 });
 
 console.log(`Listening on ${server.url}`);
